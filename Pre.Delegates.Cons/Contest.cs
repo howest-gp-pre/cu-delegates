a1de6547ba4e16e581bc6127a9cdebcba94586aa
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2022-08-29 14:32:55 +0200

    Add console project

---
+++ Pre.Delegates.Cons/Contest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pre.Delegates.Cons
+{
+public class Contest
+{
+    private const int MaxScorePerRound = 5;
+    private static readonly Random random = new Random();
+
+    public List<Participant> Participants { get; } = new List<Participant>();
+
+    public void AddParticipant(Participant participant)
+    {
+        Participants.Add(participant);
+    }
+
+    public void PlayRound()
+    {
+        foreach(Participant participant in Participants)
+        {
+            int randomScore = random.Next(MaxScorePerRound + 1);
+            participant.RegisterScore(randomScore);
+        }
+    }
+
+public Participant FindWinner()
+{
+    Participant winner = Participants[0];
+    for(int i = 1; i < Participants.Count; i++)
+    {
+        Participant participant = Participants[i];
+        if(participant.TotalScore > winner.TotalScore)
+        {
+            winner = participant;
+        }
+    }
+    return winner;
+}
+}
+}
---
+++ Pre.Delegates.Cons/Program.cs
@@ -0,0 +1,33 @@
+﻿using Pre.Delegates.Cons;
+
+// create some participants
+Participant alice = new Participant("Alice");
+Participant bob = new Participant("Bob");
+Participant carol = new Participant("Carol");
+
+// register participants to contest
+Contest contest = new Contest();
+contest.AddParticipant(alice);
+contest.AddParticipant(bob);
+contest.AddParticipant(carol);
+
+// play some rounds
+int numRounds = 3;
+for(int i = 0; i < numRounds; i++)
+{
+    contest.PlayRound();
+}
+
+// find the winner!
+Participant winner = contest.FindWinner();
+
+// print outcome
+Console.WriteLine("Participants:");
+Console.WriteLine("-------------");
+Console.WriteLine(string.Join("\n", contest.Participants));
+Console.WriteLine();
+
+Console.WriteLine("Winner:");
+Console.WriteLine("-------");
+Console.WriteLine(winner);
+
