c4c035a14f3ff4c2204b964a21594fcb9eaa7470
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2022-08-29 16:15:10 +0200

    Format code

--- Pre.Delegates.Cons/Contest.cs
+++ Pre.Delegates.Cons/Contest.cs
@@ -6,39 +6,39 @@ using System.Threading.Tasks;
 
 namespace Pre.Delegates.Cons
 {
-public class Contest
-{
-    private const int MaxScorePerRound = 5;
-    private static readonly Random random = new Random();
-
-    public List<Participant> Participants { get; } = new List<Participant>();
-
-    public void AddParticipant(Participant participant)
+    public class Contest
     {
-        Participants.Add(participant);
-    }
+        private const int MaxScorePerRound = 5;
+        private static readonly Random random = new Random();
 
-    public void PlayRound()
-    {
-        foreach(Participant participant in Participants)
+        public List<Participant> Participants { get; } = new List<Participant>();
+
+        public void AddParticipant(Participant participant)
         {
-            int randomScore = random.Next(MaxScorePerRound + 1);
-            participant.RegisterScore(randomScore);
+            Participants.Add(participant);
+        }
+
+        public void PlayRound()
+        {
+            foreach(Participant participant in Participants)
+            {
+                int randomScore = random.Next(MaxScorePerRound + 1);
+                participant.RegisterScore(randomScore);
+            }
+        }
+
+        public Participant FindWinner()
+        {
+            Participant winner = Participants[0];
+            for(int i = 1; i < Participants.Count; i++)
+            {
+                Participant participant = Participants[i];
+                if(participant.TotalScore > winner.TotalScore)
+                {
+                    winner = participant;
+                }
+            }
+            return winner;
         }
     }
-
-public Participant FindWinner()
-{
-    Participant winner = Participants[0];
-    for(int i = 1; i < Participants.Count; i++)
-    {
-        Participant participant = Participants[i];
-        if(participant.TotalScore > winner.TotalScore)
-        {
-            winner = participant;
-        }
-    }
-    return winner;
-}
-}
 }
